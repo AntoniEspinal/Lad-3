464c600a245d02bc630673a4fb7a15482171e132
AntoniEspinal <97680717+AntoniEspinal@users.noreply.github.com>
2022-12-07 11:52:40 -0500

    DIO

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private float speed = 10.0f;
     private Rigidbody playerRb;
+    
     // Start is called before the first frame update
     void Start()
     {
